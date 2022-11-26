6a9a8cc426bfa76e50dcd16057172b8ab2fe9f5d
Bogdan <noskovbogdan@gmail.com>
2022-11-26 14:24:43 +0300

    feat: add template for gql enpoint

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,18 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { graphqlBodySchema } from './schema';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.post('/', async function (request, reply) {});
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: graphqlBodySchema,
+      },
+    },
+    async function (request, reply) {}
+  );
 };
 
 export default plugin;
