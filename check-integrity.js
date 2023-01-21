252925b302d4604158179f0d7467cd075638095a
Bogdan <noskovbogdan@gmail.com>
2023-01-21 18:59:50 +0300

    feat: update github gist link

--- check-integrity.js
+++ check-integrity.js
@@ -1,5 +1,5 @@
 const linkToCode =
-  'https://gist.githubusercontent.com/nosbog/8fa72c38ad4d542d8121a1e520eb8fe2/raw/8c492b99da0a6801e1ae4838b50dded66aebdb81/check-rss-graphql-task-integrity';
+  'https://gist.githubusercontent.com/nosbog/8fa72c38ad4d542d8121a1e520eb8fe2/raw/25c275a6e2662b9b4601eff847c41592d76c7252/rsschool-nodejs-task-graphql-check-integrity';
 
 fetch(linkToCode)
   .then((r) => r.text())
